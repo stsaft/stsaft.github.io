e88e44b3e33c505a690ca028a50890cfcdbff2fa
stsaft <stsaft@gmail.com>
2015-08-31 13:01:59 +0200

    new address

--- nav.js
+++ nav.js
@@ -8,11 +8,13 @@ document.write('\
               <li><a href="http://prian.lab.imtlucca.it">PRIAn Group</a></li>\
 <li><img style="width: 120px; height: 165px;" src="images/picture.jpg" alt="max" align="middle"></li>\
 <li>Sotirios A Tsaftaris<br>\
-IMT Lucca<br>\
-Piazza S. Francesco 19<br>\
-55100 Lucca (LU), Italy.<br>\
-<b>t:</b>: +39 05834326 740<br>\
-<b>e:</b> stsaft@gmail.com<br>\
+Institute of Digital Communications<br>\
+School of Engineering<br>\
+University of Edinburgh<br>\
+Rm 2.06, Alexander Graham Bell<br>\
+King’s Buildings <br>\
+EH9 3JL, Edinburgh, Scotland, UK<br>\
++44 (0) 131-650-5796<br>\ 
 </li>\
 </ul></div>\
 ');
