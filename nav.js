1f63eeeb2d3ac5f1acd34c69d56d0cd58ea46e53
stsaft <stsaft@gmail.com>
2015-08-31 13:10:15 +0200

    Update nav.js

--- nav.js
+++ nav.js
@@ -9,6 +9,13 @@ document.write('\
 <li><img style="width: 120px; height: 165px;" src="images/picture.jpg" alt="max" align="middle"></li>\
 <li>Sotirios A Tsaftaris<br>\
 Institute of Digital Communications<br>\
+School of Engineering<br>\
+University of Edinburgh<br>\
+Rm 2.06, Alexander Graham Bell<br>\
+King’s Buildings<br>\
+EH9 3JL, Edinburgh, Scotland, UK<br>\
++44 (0) 131-650-5796<br>\ 
+S.Tsaftaris@ed.ac.uk<br>\
 </li>\
 </ul></div>\
 ');
