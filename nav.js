29c5d20694b5563677f32476f3a54f3b9f50a2ea
stsaft <stsaft@gmail.com>
2015-08-31 13:16:50 +0200

    Update nav.js

--- nav.js
+++ nav.js
@@ -7,8 +7,7 @@ document.write('\
               <li><a href="Publications.html">Publications</a></li>\
               <li><a href="http://prian.lab.imtlucca.it">PRIAn Group</a></li>\
 <li><img style="width: 120px; height: 165px;" src="images/picture.jpg" alt="max" align="middle"></li>\
-<li>Sotirios A Tsaftaris</li>\
-</ul>\
+<li>Sotirios A Tsaftaris<br>\
 Institute of Digital Communications<br>\
 School of Engineering<br>\
 University of Edinburgh<br>\
@@ -16,6 +15,6 @@ Rm 2.06, Alexander Graham Bell<br>\
 King’s Buildings<br>\
 EH9 3JL, Edinburgh, Scotland, UK<br>\
 +44 (0) 131-650-5796<br>\ 
-S.Tsaftaris@ed.ac.uk<br>\
-</div>\
+S.Tsaftaris@ed.ac.uk</li>\
+</ul></div>\
 ');
