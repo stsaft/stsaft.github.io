63686a03ccc00372c984bd3bdeced217675cba4e
stsaft <stsaft@gmail.com>
2015-08-31 13:17:49 +0200

    removed address
    removed address because it didnt like it too long
    
    <li>Sotirios A Tsaftaris<br>\
    Institute of Digital Communications<br>\
    School of Engineering<br>\
    University of Edinburgh<br>\
    Rm 2.06, Alexander Graham Bell<br>\
    King’s Buildings<br>\
    EH9 3JL, Edinburgh, Scotland, UK<br>\
    +44 (0) 131-650-5796<br>\ 
    S.Tsaftaris@ed.ac.uk</li>\

--- nav.js
+++ nav.js
@@ -7,14 +7,5 @@ document.write('\
               <li><a href="Publications.html">Publications</a></li>\
               <li><a href="http://prian.lab.imtlucca.it">PRIAn Group</a></li>\
 <li><img style="width: 120px; height: 165px;" src="images/picture.jpg" alt="max" align="middle"></li>\
-<li>Sotirios A Tsaftaris<br>\
-Institute of Digital Communications<br>\
-School of Engineering<br>\
-University of Edinburgh<br>\
-Rm 2.06, Alexander Graham Bell<br>\
-King’s Buildings<br>\
-EH9 3JL, Edinburgh, Scotland, UK<br>\
-+44 (0) 131-650-5796<br>\ 
-S.Tsaftaris@ed.ac.uk</li>\
 </ul></div>\
 ');
