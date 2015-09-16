6504782e8051032275dc0373ed9c8735e840e9ea
Sotirios Tsaftaris <sotos@Sotirioss-MacBook-Air.local>
2015-09-16 15:28:18 +0100

    new affil, and new papers

--- nav.js
+++ nav.js
@@ -5,7 +5,7 @@ document.write('\
               <li><a href="Bio.html">Biography</a></li>\
               <li><a href="Research.html">Research</a></li>\
               <li><a href="Publications.html">Publications</a></li>\
-              <li><a href="http://prian.lab.imtlucca.it">PRIAn Group</a></li>\
+              <li><a href="http://prian.imtlucca.it">PRIAn Group</a></li>\
 <li><img style="width: 120px; height: 165px;" src="images/picture.jpg" alt="max" align="middle"></li>\
 </ul></div>\
 ');
